a44093f2b81b336a2e7fb28a09770f0e1dfb7f46
Tatshiki0529 <info@ttsk3.net>
2021-11-28 12:43:18 +0900

    コンフィグファイルの追加(コピー)

---
+++ libs/config_copy.php
@@ -0,0 +1,20 @@
+<?php
+
+	/**
+	 * SkyBlockJP Configuration Script (config.php)
+	 * 
+	 * 設定を書き込むためのファイル。
+	 * 
+	 * @access private
+	 * @author Tateshiki0529
+	 * @copyright (C) 2021- Tateshiki Lab. All Rights Reserved.
+	 * @category Configuration
+	 * @package SkyBlockJP
+	**/
+
+	// Hypixel の設定
+	define('HYPIXEL_API_KEY', '[Hypixel API Key]');
+	define('HYPIXEL_API_ENDPOINT', 'https://api.hypixel.net');
+	
+	// Mojang API の設定
+	define('MOJANG_API_ENDPOINT', 'https://api.mojang.com');
